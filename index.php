3d31341bd39da7fd49f3bb02763d1fc13cf6b728
Christian Cataldo <christian.cataldo@itcollege.ee>
2019-03-28 12:03:13 +0200

    Add index page and script

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include "../home/header.php";
+?>
+
+<h1 id="title">Blackjack</h1>
+<p id="text-area"></p>
+<br>
+
+<button id="new-game-button">New Game!</button>
+<button id="hit-button">Get another card!</button>
+<button id="stay-button">Find who wins!</button>
+
+<script src="script.js"></script>
+
+</body>
+</html>
+
+ 
